7de6d7f049e3508a6c45b5a27f93bb805f3d3188
Thiago Silva <thiago25pro@gmail.com>
2020-10-10 04:18:15 -0300

    15:42, before adding images

---
+++ src/DefaultOverlayContent/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+`;
+
+export const Heading = styled.div`
+
+    
+
+`
+export const Buttons = styled.div`
+
+    
+
+`
---
+++ src/components/Page/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+import { ModelsWrapper, ModelSection } from '../Model';
+import DefaultOverlayContent from '../DefaultOverlayContent';
+
+const Page: React.FC = () => {
+  return (
+    <Container>
+      
+      <ModelsWrapper />
+
+        <div>
+
+          <ModelSection
+            modelName="Model One"
+            overlayNode={
+              <DefaultOverlayContent
+                label="Model One"
+                description="Order Online for Delivery"
+              />
+            }
+          />
+
+        </div>
+
+      </ModelsWrapper>
+
+    </Container>
+  );
+};
+
+export default Page;
