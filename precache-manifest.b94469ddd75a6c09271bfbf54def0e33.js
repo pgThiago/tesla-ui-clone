656269c8bb35a1c03d42a513a0261f1197c4d8fe
Thiago Silva <thiago25pro@gmail.com>
2020-10-11 21:51:28 -0300

    Updates

---
+++ precache-manifest.b94469ddd75a6c09271bfbf54def0e33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24642cbfee96d5e3492b3690010d71dc",
+    "url": "/tesla-ui-clone/index.html"
+  },
+  {
+    "revision": "59f053aeff19eaa273dd",
+    "url": "/tesla-ui-clone/static/js/2.92df4f00.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/tesla-ui-clone/static/js/2.92df4f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "401a55ee80eefbbc5c51",
+    "url": "/tesla-ui-clone/static/js/main.6f351647.chunk.js"
+  },
+  {
+    "revision": "d47490a942a3428de69f",
+    "url": "/tesla-ui-clone/static/js/runtime-main.6e50dad0.js"
+  }
+]);
